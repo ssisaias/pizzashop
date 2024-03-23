10d1d2a2d1a34d9ed4cb89b6512d1275b76e6a62
ssisaias <ssisaias@users.noreply.github.com>
2024-03-23 19:59:59 -0300

    Adds the login form
    - adds new ui components
    - Changes application theme

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
-
 export function AuthLayout() {
   return (
-    <div>
-      <h1>autenticacao</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="tetx-lg flex items-center gap-3 font-medium text-foreground">
+          <Pizza className="h-5 w-5"></Pizza>
+          <span className="font-semibold">PizzaShop</span>
+        </div>
+        <footer>
+          Painel do parceiro &copy; pizzashop {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet></Outlet>
       </div>
     </div>
