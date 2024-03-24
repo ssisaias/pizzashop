78ac20026b406b1edf3d94e33acc73ce62f5a272
ssisaias <ssisaias@users.noreply.github.com>
2024-03-23 23:40:16 -0300

    App layout with header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '@/components/Header/Header'
+
 export function AppLayout() {
   return (
-    <div>
-      <h1>cabecalho</h1>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header></Header>
 
-      <div>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet></Outlet>
       </div>
     </div>
--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,7 +2,7 @@ import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="tetx-lg flex items-center gap-3 font-medium text-foreground">
           <Pizza className="h-5 w-5"></Pizza>
