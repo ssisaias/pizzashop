3dcf0daf21f8c208dde4e4342d5b813f1ee4f374
ssisaias <ssisaias@users.noreply.github.com>
2024-03-25 16:01:25 -0300

    Adda navigation button (pagination)

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
-import { Orders } from './pages/app/orders/oders'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/signin'
 import { SignUp } from './pages/auth/signup'
 
