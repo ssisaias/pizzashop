f8c062969c95fd8f4751dce629a7c455d341000e
ssisaias <ssisaias@users.noreply.github.com>
2024-03-25 16:40:09 -0300

    Adds dashboard cards

---
+++ src/pages/app/Dashboard/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DailyOrdersAmountCard } from './daily-orders-amount-card'
+import { MonthlyCanceledOrdersCard } from './monthly-canceled-orders'
+import { MonthOrdersAmountCard } from './monthly-orders-amount-card'
+import { MonthlyRevenueCard } from './monthly-revenue-card'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthlyRevenueCard />
+          <MonthOrdersAmountCard />
+          <DailyOrdersAmountCard />
+          <MonthlyCanceledOrdersCard />
+        </div>
+      </div>
+    </>
+  )
+}
