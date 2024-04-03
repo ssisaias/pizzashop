54866ae7334c11b809de39970b86b4a3df984123
ssisaias <ssisaias@users.noreply.github.com>
2024-04-02 22:34:50 -0300

    Adds API call for the revenue in period chart
    - adds the date range picker and configure the data to display on
    it correctly

--- src/pages/app/Dashboard/revenue-chart.tsx
+++ src/pages/app/Dashboard/revenue-chart.tsx
@@ -1,3 +1,7 @@
+import { subDays } from 'date-fns'
+import { useMemo, useState } from 'react'
+import { DateRange } from 'react-day-picker'
+import { useQuery } from 'react-query'
 import {
   CartesianGrid,
   Line,
@@ -9,6 +13,7 @@ import {
 } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period'
 import {
   Card,
   CardContent,
@@ -16,18 +21,31 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
-const mockData = [
-  { date: '10/12', revenue: 1200 },
-  { date: '11/12', revenue: 500 },
-  { date: '12/12', revenue: 600 },
-  { date: '13/12', revenue: 111 },
-  { date: '14/12', revenue: 69 },
-  { date: '15/12', revenue: 1500 },
-  { date: '16/12', revenue: 1 },
-]
+import { DatePickerWithRange } from '@/components/ui/date-range-picker'
+import { Label } from '@/components/ui/label'
 
 export function RevenueChart() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  })
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((item) => ({
+      date: item.date,
+      receipt: item.receipt / 100,
+    }))
+  }, [dailyRevenueInPeriod])
+
   return (
     <Card className="col-span-6">
       <CardHeader className="flex-row items-center justify-between pb-8">
@@ -37,33 +55,45 @@ export function RevenueChart() {
           </CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
         </div>
+
+        <div className="flex items-center gap-3">
+          <Label>Período</Label>
+          <DatePickerWithRange date={dateRange} onDateChange={setDateRange} />
+        </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <LineChart data={mockData} style={{ fontSize: '12' }}>
-            <XAxis dataKey={'date'} tickLine={false} axisLine={false} dy={15} />
-            <YAxis
-              stroke="#888"
-              axisLine={false}
-              tickLine={false}
-              width={90}
-              tickFormatter={(value: number) => {
-                return `${value.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })}`
-              }}
-            />
-            <CartesianGrid vertical={false} className="stroke-muted" />
-            <Line
-              type={'linear'}
-              strokeWidth={2}
-              dataKey={'revenue'}
-              stroke={colors.violet[500]}
-            />
-            <Tooltip labelStyle={{ color: colors.slate[400] }}></Tooltip>
-          </LineChart>
-        </ResponsiveContainer>
+        {chartData && (
+          <ResponsiveContainer width="100%" height={240}>
+            <LineChart data={chartData} style={{ fontSize: '12' }}>
+              <XAxis
+                dataKey={'date'}
+                tickLine={false}
+                axisLine={false}
+                dy={15}
+              />
+              <YAxis
+                stroke="#888"
+                axisLine={false}
+                tickLine={false}
+                width={90}
+                tickFormatter={(value: number) => {
+                  return `${value.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })}`
+                }}
+              />
+              <CartesianGrid vertical={false} className="stroke-muted" />
+              <Line
+                type={'linear'}
+                strokeWidth={2}
+                dataKey={'receipt'}
+                stroke={colors.violet[500]}
+              />
+              <Tooltip labelStyle={{ color: colors.slate[400] }}></Tooltip>
+            </LineChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   )
