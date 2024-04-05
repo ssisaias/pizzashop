2a1c1fd19f8210df541a3d4021a227c243fe0d1c
ssisaias <ssisaias@users.noreply.github.com>
2024-04-04 22:28:17 -0300

    example for a unit testing on the Order Status

--- src/components/OrderStatus/order-status.spec.tsx
+++ src/components/OrderStatus/order-status.spec.tsx
@@ -1,3 +1,31 @@
-test('1+1 equals 2', () => {
-  expect(1 + 1).toBe(2)
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from './order-status'
+
+describe('Order status', () => {
+  it('should display the right text based on order status', () => {
+    /* Pending */
+    let wrapper = render(<OrderStatus status="pending" />)
+
+    // this displays the redenred component in the console
+    // wrapper.debug()
+
+    expect(wrapper.getByText('Pendente')).toBeInTheDocument()
+
+    /* Canceled */
+    wrapper = render(<OrderStatus status="canceled" />)
+    expect(wrapper.getByText('Cancelado')).toBeInTheDocument()
+
+    /* Delivered */
+    wrapper = render(<OrderStatus status="delivered" />)
+    expect(wrapper.getByText('Entregue')).toBeInTheDocument()
+
+    /* Delivering */
+    wrapper = render(<OrderStatus status="delivering" />)
+    expect(wrapper.getByText('Em entrega')).toBeInTheDocument()
+
+    /* processing */
+    wrapper = render(<OrderStatus status="processing" />)
+    expect(wrapper.getByText('Em preparo')).toBeInTheDocument()
+  })
 })
