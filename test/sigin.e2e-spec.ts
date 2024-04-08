f628bdfe8a0e111eeac298345e432f1ff660b1f1
ssisaias <ssisaias@users.noreply.github.com>
2024-04-07 23:14:03 -0300

    Fix e2e test mode checking

--- test/sigin.e2e-spec.ts
+++ test/sigin.e2e-spec.ts
@@ -6,9 +6,7 @@ test('sign in successfully', async ({ page }) => {
   await page.getByLabel('E-mail').fill('john.doe@example.com')
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
-  const toast = page.getByText(
-    'Enviamos um link de autenticação para o seu email',
-  )
+  const toast = page.getByText('Clique para ir ao dashboard')
 
   expect(toast).toBeVisible()
 
